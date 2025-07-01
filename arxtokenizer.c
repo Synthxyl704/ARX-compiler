5bea5e6fe6b148ce866d29c35f4afc811447a5f9
IsoAris -RESURRECTION- <159614599+Synthxyl704@users.noreply.github.com>
2025-07-02 02:54:46 +0530

    Added support for datatype recognition, lexing and scientific notation

--- arxtokenizer.c
+++ arxtokenizer.c
@@ -1,160 +1,232 @@
 #include "tokenizer.h"
+#include <stdbool.h>
 
-// need to know when to extern them or else its gonna break again
 token tokenArray[MAX_TOKENS_USAGE];
+
 char alphaDetectionBuffer[MAX_BUFFER_SIZE] = {0};
 char linearOneLineSize[0x400];      // 1024
+
 int bufferIndex = 0;
 int tokenCount = 0;
 int lineNumber = 1;
 
 void finalizeAlphaToken() {
-
-  /*
-   
-  if (bufferIndex > 0 && tokenCount < MAX_TOKENS_USAGE) {
-    alphaDetectionBuffer[bufferIndex] = '\0'; 
-    tokenArray[tokenCount].datatype = strcmp(alphaDetectionBuffer, "_ret") == 0 ? _return 
-                                                                                : variable;
-
-    if (alphaDetectionBuffer[bufferIndex - 1] != isspace(alphaDetectionBuffer[bufferIndex - 1])) { 
-      tokenArray[tokenCount].datatype = isdigit(alphaDetectionBuffer[bufferIndex - 1]) ? intLiteral : variable;
-    } 
-
-    */
-
   if (bufferIndex > 0 && tokenCount < MAX_TOKENS_USAGE) {
     alphaDetectionBuffer[bufferIndex] = '\0';
+
+    if (strcmp(alphaDetectionBuffer, "_prov") == 0) {
+      tokenArray[tokenCount].datatype = _main;
+    } 
     
-    if (strcmp(alphaDetectionBuffer, "_ret") == 0) {
-        tokenArray[tokenCount].datatype = _return;
+    else if (strcmp(alphaDetectionBuffer, "_ret") == 0) {
+      tokenArray[tokenCount].datatype = _return;
+    } 
+    
+    else if (strcmp(alphaDetectionBuffer, "mv_int") == 0) {
+      tokenArray[tokenCount].datatype = intDatatype;
     } 
 
-    else if(strstr(alphaDetectionBuffer, "ret") != NULL) {
-        // tokenArray[tokenCount].datatype = variable;
-        fprintf(stderr, "\nstd:syntax:error::[TC%d/L%d]::[%s]:::[_ret]\n", tokenCount, lineNumber, alphaDetectionBuffer, "_ret");
-        tokenArray[tokenCount].datatype = variable;  // Still tokenize it as variable
-      } 
+    else if (strcmp(alphaDetectionBuffer, "mv_flt") == 0) {
+      tokenArray[tokenCount].datatype = floatDatatype;
+    } 
+    
+    else if (strstr(alphaDetectionBuffer, "prov") != NULL) {
+      fprintf(stderr, "\nstd:syntax_error::[TC%d/L%d]::[%s]:::[%s]", tokenCount, lineNumber, alphaDetectionBuffer, "_prov");
+      tokenArray[tokenCount].datatype = variable;
+    } 
+    
+    else if (strstr(alphaDetectionBuffer, "ret") != NULL) {
+      fprintf(stderr, "\nstd:syntax_error::[TC%d/L%d]::[%s]:::[%s]\n\n", tokenCount, lineNumber, alphaDetectionBuffer, "_ret");
+      tokenArray[tokenCount].datatype = variable;
+    } else {
 
-    else {
-      int allDigits = 1;
-        for (int inc = 0; inc < bufferIndex; inc += 1) {
-          if (!isdigit(alphaDetectionBuffer[inc])) {
-            allDigits = 0;
-            break;
-          }  // why is this shit ending before the keyword LOL
+
+  // scientific/exponentiational notation support
+  int hasViewedE = 0;
+  int E_Position = -1;
+
+  for (int inc = 0; inc < bufferIndex; inc += 1) {
+    if (alphaDetectionBuffer[inc] == 'E' || alphaDetectionBuffer[inc] == 'e') {
+      hasViewedE = 1;
+      E_Position = inc;
+
+      break;
+    }
+  }
+
+  if (hasViewedE) {
+    int allDigitsB4E = 1;
+    int hasSeenDecimalDot = 0;
+    for (int inc = 0; inc < E_Position; inc += 1) {
+      if (alphaDetectionBuffer[inc] == '.') {
+        hasSeenDecimalDot += 1;
+        if (hasSeenDecimalDot > 1) {
+          allDigitsB4E = 0;
+          fprintf(stderr, "\nstd:syntax_error::[TC%d/L%d]::[%s]:::[%s]\n\n", tokenCount, lineNumber, alphaDetectionBuffer, "multiple_decimal_digits=invalid");
+          
+          break;
         }
-        
-      if (allDigits) {
-        tokenArray[tokenCount].datatype = intLiteral;
-      } else {
-        tokenArray[tokenCount].datatype = variable;                              // note to self: cannot rewrite [else if] statements after [else] is already written (wdift)
+      } else if (!isdigit((unsigned char)alphaDetectionBuffer[inc])) {
+        allDigitsB4E = 0;
+        break;
       }
     }
-    
-    strcpy(tokenArray[tokenCount].lexicalBuffer, alphaDetectionBuffer);          // copy finalized token to alphaBuffer for (a) finalized reading
+
+    int allDigitsAfterE = 1;
+
+    if (E_Position == bufferIndex - 1) {
+      allDigitsAfterE = 0; // nothing after E/e
+    } 
+
+    for (int inc = E_Position + 1; inc < bufferIndex; inc += 1) {
+      if (!isdigit((unsigned char)alphaDetectionBuffer[inc])) {
+        allDigitsAfterE = 0;
+        break;
+      }
+    }
+
+    if (allDigitsAfterE && allDigitsB4E) {
+      if (hasSeenDecimalDot == 0) {
+        tokenArray[tokenCount].datatype = intLiteral;
+      } else if (hasSeenDecimalDot == 1) {
+        tokenArray[tokenCount].datatype = floatingPointLiteral;
+      } else {
+        tokenArray[tokenCount].datatype = variable;
+      }
+    } else {
+      tokenArray[tokenCount].datatype = variable;
+    }
+  }
+
+  else { // only runs if NOT scientific notation - balances
+    int8_t allDigits = 1;
+    int8_t hasSeenDecimalDot = 0;
+    for (int inc = 0; inc < bufferIndex; inc += 1) {
+      if (alphaDetectionBuffer[inc] == '.') {
+        hasSeenDecimalDot += 1;
+      } else if (!isdigit((unsigned char)alphaDetectionBuffer[inc])) {
+        allDigits = 0;
+        break;
+      }
+    }
+    if (allDigits == 1 && hasSeenDecimalDot == 0) {
+      tokenArray[tokenCount].datatype = intLiteral;
+    } else if (allDigits == 1 && hasSeenDecimalDot == 1) {
+      tokenArray[tokenCount].datatype = floatingPointLiteral;
+    } else {
+      tokenArray[tokenCount].datatype = variable;
+    }
+  }
+}
+
+    strcpy(tokenArray[tokenCount].lexicalBuffer, alphaDetectionBuffer);
     tokenCount += 1;
 
-    bufferIndex = 0;              
-    memset(alphaDetectionBuffer, 0, sizeof(alphaDetectionBuffer));               // reset the buffer back -- memset(where, how_much, sizeof());
+    bufferIndex = 0;
+    memset(alphaDetectionBuffer, 0, sizeof(alphaDetectionBuffer));
   }
 }
 
 void tokenize(char charFromTheFile) {
   if (isspace(charFromTheFile)) {
     finalizeAlphaToken();
-  }
-
-  else if (isalpha(charFromTheFile) || charFromTheFile == '_' || (bufferIndex > 0 && isalnum(charFromTheFile))) {
+  } else if (isalpha(charFromTheFile) || charFromTheFile == '_' || (bufferIndex > 0 && isalnum(charFromTheFile))) {
     if (bufferIndex < MAX_BUFFER_SIZE - 1) {
       alphaDetectionBuffer[bufferIndex] = charFromTheFile;
       bufferIndex += 1;
     }
-  }
-
-  /* ---------------------------------------------------------------- //
- 
-      else if (isalpha(charFromTheFile)) {
-        alphaDetectionBuffer[bufferIndex += 1] = charFromTheFile; 
-      }
-  // ---------------------------------------------------------------- */ 
-  
-  else if(isdigit(charFromTheFile)) {
+  } else if (isdigit(charFromTheFile)) {
     alphaDetectionBuffer[bufferIndex] = charFromTheFile;
     bufferIndex += 1;
-  }
-
-  // -----------------
-  /* OPERATORS HERE */
-  
-  // if its a semicolon ;
-  else if (charFromTheFile == ';' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == ';' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = semicolon;
     tokenArray[tokenCount].lexicalBuffer[0] = ';';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-
-  // if its a plus operator +
-  else if (charFromTheFile == '+' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '+' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = plus;
     tokenArray[tokenCount].lexicalBuffer[0] = '+';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-  
-  // if its a minus operator - ?
-  else if (charFromTheFile == '-' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '-' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = minus;
     tokenArray[tokenCount].lexicalBuffer[0] = '-';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-  
-  // if its a multiplication operator *
-  else if (charFromTheFile == '*' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '*' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = multiply;
     tokenArray[tokenCount].lexicalBuffer[0] = '*';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-  
-  // if its a division operator /
-  else if (charFromTheFile == '/' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '/' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = divide;
     tokenArray[tokenCount].lexicalBuffer[0] = '/';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-  
-  // if its a modulo operator %
-  else if (charFromTheFile == '%' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '%' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = modulo;
     tokenArray[tokenCount].lexicalBuffer[0] = '%';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  }
-  
-  // if its an assignment operator =
-  else if (charFromTheFile == '=' && tokenCount < MAX_TOKENS_USAGE) {
+  } else if (charFromTheFile == '=' && tokenCount < MAX_TOKENS_USAGE) {
     finalizeAlphaToken();
     tokenArray[tokenCount].datatype = assignment;
     tokenArray[tokenCount].lexicalBuffer[0] = '=';
     tokenArray[tokenCount].lexicalBuffer[1] = '\0';
     tokenCount += 1;
-  } // pretty repetitive but at least now its concrete
+  } else if (charFromTheFile == '(' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = LHcircleBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = '(';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  } else if (charFromTheFile == ')' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = RHcircleBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = ')';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  } else if (charFromTheFile == '{' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = LHcurlyBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = '{';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  } else if (charFromTheFile == '}' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = RHcurlyBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = '}';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  } else if (charFromTheFile == '[' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = LHsquareBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = '[';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  } else if (charFromTheFile == ']' && tokenCount < MAX_TOKENS_USAGE) {
+    finalizeAlphaToken();
+    tokenArray[tokenCount].datatype = RHsquareBracket;
+    tokenArray[tokenCount].lexicalBuffer[0] = ']';
+    tokenArray[tokenCount].lexicalBuffer[1] = '\0';
+    tokenCount += 1;
+  }
 }
 
 /* temporary parser confirmation */
 void printTokens(void) {
   for (int inc = 0; inc < tokenCount; inc += 1) {
-    printf("token [%d]: type = [%d], value = [%s]\n", inc, tokenArray[inc].datatype, tokenArray[inc].lexicalBuffer);
+    printf("token [%d]: type = [%s], value = [%s]\n",
+      inc,
+      tokenTypeToStr(tokenArray[inc].datatype),
+      tokenArray[inc].lexicalBuffer
+    );
   }
 }
 
