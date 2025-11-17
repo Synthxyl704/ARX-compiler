72c5ab18ee7dc9b55c9ec5569bf0effb80dee94d
7Tiamatr/x_ <159614599+Synthxyl704@users.noreply.github.com>
2025-11-17 16:50:24 +0530

    optimized conditional control structure to parallel array relational mapping
    now its way cleaner, way better to implement new stuff and reduced code size, yay

--- arxtokenizer.c
+++ arxtokenizer.c
@@ -102,98 +102,63 @@ void finalizeAlphaToken(void) {
     if (bufferIndex > 0 && tokenCount < MAX_TOKENS_USAGE) {
         alphaDetectionBuffer[bufferIndex] = '\0';
 
-        if (strcmp(alphaDetectionBuffer, "_prov") == 0) {
-            tokenArray[tokenCount].datatype = _main;
-            goto store_token; // IM SOS ORRY I DONT KNWO3N3 WHAT ELSEFTO DO 2
+        // known tokens
+        // read tokens
+        // if known = read = that d_t
+        const char *knownTokens[] = {
+            "_prov", "_ret", "_subprog", "subScope", "extScope",
+            "mv_int", "mv_flt", "mv_chr", "mv_dbl", "mv_class" // this looks way clear too, ty @riz.dscrd
+        };
+
+        // enum tokentype { ... }
+        enum tokenType knownDatatypes[] = {
+            _main, _return, _subprog, subScope, extScope,
+            intDatatype, floatDatatype, charDatatype, doubleDatatype, classDatatype
+        };
+
+        // entire array size / one
+        // (10 apples) / (1 apple) = (10){count}
+        int numKnownTokens = sizeof(knownTokens) / sizeof(knownTokens[0]);
+        signed int matchedIndex = (-1);
+        
+        for (int inx = 0; inx < numKnownTokens; inx += 1) {
+            if (strcmp(alphaDetectionBuffer, knownTokens[inx]) == 0) {
+                matchedIndex = inx;
+                break;
+            }
         }
-        if (strcmp(alphaDetectionBuffer, "_ret") == 0) {
-            tokenArray[tokenCount].datatype = _return;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "_subprog") == 0) {
-            tokenArray[tokenCount].datatype = _subprog;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "subScope") == 0) {
-            tokenArray[tokenCount].datatype = subScope;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "extScope") == 0) {
-            tokenArray[tokenCount].datatype = extScope;
+
+        if (matchedIndex != (-1)) {
+            tokenArray[tokenCount].datatype = knownDatatypes[matchedIndex];
             goto store_token;
         }
 
-        if (strcmp(alphaDetectionBuffer, "mv_int") == 0) {
-            tokenArray[tokenCount].datatype = intDatatype;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "mv_flt") == 0) {
-            tokenArray[tokenCount].datatype = floatDatatype;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "mv_chr") == 0) {
-            tokenArray[tokenCount].datatype = charDatatype;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "mv_dbl") == 0) {
-            tokenArray[tokenCount].datatype = doubleDatatype;
-            goto store_token;
-        }
-        if (strcmp(alphaDetectionBuffer, "mv_class") == 0) {
-            tokenArray[tokenCount].datatype = classDatatype;
-            goto store_token;
-        }
+        // -------------- SYNTAX ERRORS -----------------
 
-        // if (strcmp(alphaDetectionBuffer, "mv_int*") == 0) {
-        //     tokenArray[tokenCount].datatype = intPointerDatatype;
-        //     goto store_token;
-        // }
-        // if (strcmp(alphaDetectionBuffer, "mv_flt*") == 0) {
-        //     tokenArray[tokenCount].datatype = floatPointerDatatype;
-        //     goto store_token;
-        // }
-        // if (strcmp(alphaDetectionBuffer, "mv_dbl*") == 0) {
-        //     tokenArray[tokenCount].datatype = doublePointerDatatype;
-        //     goto store_token;
-        // }
-        // if (strcmp(alphaDetectionBuffer, "mv_chr*") == 0) {
-        //     tokenArray[tokenCount].datatype = charPointerDatatype;
-        //     goto store_token;
-        // }
-
-        // if (strcmp(alphaDetectionBuffer, "mv_class*") == 0) {
-        //     tokenArray[tokenCount].datatype = classPointerDatatype;
-        //     goto store_token;
-        // }
-
-        if (strstr(alphaDetectionBuffer, "prov") != NULL && strcmp(alphaDetectionBuffer, "_prov") != 0) {
-            reportSyntaxError(alphaDetectionBuffer, "_prov", "use underscore prefix for keywords");
+        // syntax error handling, should a parser make this more efficient?
+        if (strstr(alphaDetectionBuffer, "prov") && strcmp(alphaDetectionBuffer, "_prov") != 0) {
+            reportSyntaxError(alphaDetectionBuffer, "_prov", "prepend underscore (_) prefix for main/prov keyword");
             tokenArray[tokenCount].datatype = variable;
             goto store_token;
         }
-        if (strstr(alphaDetectionBuffer, "ret") != NULL && strcmp(alphaDetectionBuffer, "_ret") != 0) {
-            reportSyntaxError(alphaDetectionBuffer, "_ret", "use underscore prefix for return keyword");
+        if (strstr(alphaDetectionBuffer, "ret") && strcmp(alphaDetectionBuffer, "_ret") != 0) {
+            reportSyntaxError(alphaDetectionBuffer, "_ret", "prepend underscore (_) prefix for return keyword");
             tokenArray[tokenCount].datatype = variable;
             goto store_token;
         }
-        if (strstr(alphaDetectionBuffer, "subprog") != NULL && strcmp(alphaDetectionBuffer, "_subprog") != 0) {
-            reportSyntaxError(alphaDetectionBuffer, "_subprog", "use underscore prefix for function keyword");
+        if (strstr(alphaDetectionBuffer, "subprog") && strcmp(alphaDetectionBuffer, "_subprog") != 0) {
+            reportSyntaxError(alphaDetectionBuffer, "_subprog", "prepend underscore (_) prefix for function keyword");
             tokenArray[tokenCount].datatype = variable;
             goto store_token;
         }
-
         if ((strstr(alphaDetectionBuffer, "mvint") != NULL || strstr(alphaDetectionBuffer, "mv_int") != NULL) &&
-            strcmp(alphaDetectionBuffer, "mv_int") != 0
-            // strcmp(alphaDetectionBuffer, "mv_int *") != 0 &&
-            // strcmp(alphaDetectionBuffer, "mv_int*") != 0
-            // this is not going to work this way, i need to learn AST
-            ) {
-            reportSyntaxError(alphaDetectionBuffer, "mv_int", "correct syntax is mv_int for integer datatype");
+            strcmp(alphaDetectionBuffer, "mv_int") != 0) {
+            reportSyntaxError(alphaDetectionBuffer, "mv_int", "syntax seems wrong, did you mean \"mv_int?\"");
             tokenArray[tokenCount].datatype = variable;
             goto store_token;
         }
 
-        int ePos = (-1);
+        int ePos = -1;
         for (int inx = 0; inx < bufferIndex; inx += 1) {
             if (alphaDetectionBuffer[inx] == 'E' || alphaDetectionBuffer[inx] == 'e') {
                 ePos = inx;
@@ -204,35 +169,34 @@ void finalizeAlphaToken(void) {
         if (ePos != -1) {
             if (isValidScientificNotation(alphaDetectionBuffer, bufferIndex)) {
                 bool hasDecimal = false;
-                for (int inx = 0; inx < ePos; inx++) {
-                    if (alphaDetectionBuffer[inx] == '.') { hasDecimal = true; break; }
-                }
-                tokenArray[tokenCount].datatype = hasDecimal ? floatingPointLiteral : intLiteral;
+                for (int inx = 0; inx < ePos; inx += 1) {
+                    if (alphaDetectionBuffer[inx] == '.') {
+                        hasDecimal = true;
+                        break;
+                    }
+                } tokenArray[tokenCount].datatype = hasDecimal ? floatingPointLiteral : intLiteral;
             } else {
-                reportSyntaxError(alphaDetectionBuffer, "valid scientific notation",
-                                  "format: [digits][.digits]E[+/-][digits]");
-                tokenArray[tokenCount].datatype = variable;
-            }
-
-            goto store_token;
-        }
+                reportSyntaxError(
+                    alphaDetectionBuffer, 
+                    "valid scientific notation",
+                    "format: [digits][.digits]E[+/-][digits]"
+                ); tokenArray[tokenCount].datatype = variable;
+            } goto store_token;
+        } // this is so shit i hope i never work with this again
 
         bool allDigitsOrDot = true;
         int dotCount = 0;
         bool startsWithDigit = isdigit((unsigned char)alphaDetectionBuffer[0]);
 
-        for (int inx = 0; inx < bufferIndex; (inx)++) /* i hate unary increment, its like lemonade without sugar or something */ {
-            if (alphaDetectionBuffer[inx] == '.') {   // we should call [++var] as "backshot increment" instead LOL
-                dotCount += 1;                        // that means [var++] is missionary XD
+        for (int inx = 0; inx < bufferIndex; inx += 1) {
+            if (alphaDetectionBuffer[inx] == '.') {
+                dotCount += 1; // i like longhand notational equivalents
                 if (dotCount > 1) {
-                    reportSyntaxError(alphaDetectionBuffer, "single decimal point",
-                                                            "remove extra decimal points"
-                    );
-
+                    reportSyntaxError(alphaDetectionBuffer, "single decimal point", "remove extra decimal points");
                     allDigitsOrDot = false;
                     break;
                 }
-            } else if (!isdigit(/* its a shock this is even valid */(unsigned char)alphaDetectionBuffer[inx])) {
+            } else if (!isdigit((unsigned char)alphaDetectionBuffer[inx])) {
                 allDigitsOrDot = false;
                 break;
             }
@@ -244,27 +208,29 @@ void finalizeAlphaToken(void) {
         }
 
         if (allDigitsOrDot && dotCount == 1 && startsWithDigit) {
-            if (alphaDetectionBuffer[bufferIndex-1] == '.') {
-                reportWarning(alphaDetectionBuffer,
-                              "incomplete float literal - add digits after decimal point");
+            if (alphaDetectionBuffer[bufferIndex - 1] == '.') {
+                reportWarning(alphaDetectionBuffer, "incomplete float literal - add digits after decimal point");
             }
             tokenArray[tokenCount].datatype = floatingPointLiteral;
             goto store_token;
         }
 
         if (startsWithDigit && !allDigitsOrDot) {
-            reportSyntaxError(alphaDetectionBuffer, "valid identifier",
-                              "identifiers cannot start with digits");
+            reportSyntaxError(alphaDetectionBuffer, "valid identifier", "identifiers cannot start with digits");
         }
+
         tokenArray[tokenCount].datatype = variable;
 
     store_token:
         strcpy(tokenArray[tokenCount].lexicalBuffer, alphaDetectionBuffer);
-        tokenCount += 1;
+        (tokenCount)++; // like look at this stupid unary increment its so bad
         bufferIndex = 0;
+        
+        // memset(memblock, value, byteSize);
         memset(alphaDetectionBuffer, 0, sizeof(alphaDetectionBuffer));
     }
 }
+
 
 void tokenize(char charFromTheFile) {
     // static bool inCharLiteral = false;
@@ -292,13 +258,15 @@ void tokenize(char charFromTheFile) {
     if (isalpha(charFromTheFile) || charFromTheFile == '_' ||
         (bufferIndex > 0 && isalnum(charFromTheFile))) {
         if (bufferIndex < MAX_BUFFER_SIZE - 1) {
-            alphaDetectionBuffer[bufferIndex++] = charFromTheFile;
+            alphaDetectionBuffer[bufferIndex++] = charFromTheFile; // [++bufferIndex]
         } else {
-            reportSyntaxError("token_too_long", "shorter_identifier",
-                              "maximum_token_length_exceeded");
-        }
-
-        return;
+            // void reportSyntaxError(const char *found, const char *expected, const char *suggestion);
+            reportSyntaxError( 
+                "token_too_long", 
+                "shorter_identifier",
+                "maximum_token_length_exceeded"
+            );
+        } return;
     }
 
     if (isdigit(charFromTheFile)) {
@@ -327,7 +295,6 @@ void tokenize(char charFromTheFile) {
         case '{': case '}': case '[': case ']': {
             finalizeAlphaToken();
 
-            // this is absolute cinema
             enum tokenType tex = (
                 charFromTheFile == ',' ? comma :
                 charFromTheFile == ';' ? semicolon :
@@ -344,16 +311,18 @@ void tokenize(char charFromTheFile) {
                 charFromTheFile == '[' ? LHsquareBracket : RHsquareBracket
             );
 
+            // not the LaTeX's { TeX }
             tokenArray[tokenCount].datatype = tex;
             tokenArray[tokenCount].lexicalBuffer[0] = charFromTheFile;
             tokenArray[tokenCount].lexicalBuffer[1] = '\0';
             tokenCount += 1;
+
             break;
         }
 
         default:
             finalizeAlphaToken();
-            reportWarning(&charFromTheFile, "unknown character ignored");
+            reportWarning(&charFromTheFile, "unknown_character_eluded");
             break;
     }
 }
